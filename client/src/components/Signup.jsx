acbd6b2fcb4352f08cde900638ea2f37548ab77f
Wasim Raja Middya <wasimmiddya21@gmail.com>
2023-09-18 11:30:07 +0530

    Modified signup component

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import SingleInput from './SingleInput'
 
 function Signup() {
     // Initialize state to store form input values
-    const [formInput, setFormInput] = useState({})
-
-
-    useEffect(() => {
-        // Effect hook runs on component mount
-        // It can be used for any side effects, but here it's currently commented out
-    }, [])
+    const [formInput, setFormInput] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        password: ''
+    })
 
     // Update the formInput state when input fields change
     const handleInputChange = (e) => {
+
         setFormInput({
-            ...formInput, 
+            ...formInput,
             [e.target.name]: e.target.value
         })
+        // console.log(formInput);
     }
 
     // Logic for handling form submission 
@@ -32,14 +33,28 @@ function Signup() {
                 headers: {
                     "Content-type": "application/json",
                 },
-                body: JSON.stringify(formInput), // The actual user information such name, email,password etc.
+                // The actual user information such name, email,password etc.
+                body: JSON.stringify(formInput),
             })
 
-            return response.text()
+
+            return response.json()
         }
 
+        setFormInput({
+            fname: '',
+            lname: '',
+            email: '',
+            password: ''
+        })
+        console.log(formInput);
+
         // Log the response from the server
-        postData().then(d=>console.log(d))
+        postData().then((res) => {
+            if (res !== undefined) {
+                console.log(res)
+            }
+        })
     }
 
     // JSX for rendering the signup form
@@ -53,14 +68,15 @@ function Signup() {
                     <fieldset className='border-2 py-4 px-6 rounded-md' >
                         <legend className='text-left mx-6 py-2 border-2 rounded-md font-semibold font-montserrat text-slate-500 px-5'>SignUp</legend>
                         <div className='flex flex-col items-center justify-center space-y-4 w-full md:flex-row md:space-x-2 md:space-y-0'>
-                            <SingleInput fieldName='fname' fieldType='text' fieldLabel='First Name' inputChange={handleInputChange} inputValue={formInput.value} />
-                            <SingleInput fieldName='lname' fieldType='text' fieldLabel='Last Name' inputChange={handleInputChange} inputValue={formInput.value} />
+                            <SingleInput fieldName='fname' fieldType='text' fieldLabel='First Name' inputChange={handleInputChange} inputValue={formInput.fname} />
+                            
+                            <SingleInput fieldName='lname' fieldType='text' fieldLabel='Last Name' inputChange={handleInputChange} inputValue={formInput.lname} />
                         </div>
                         <div className='my-3'>
-                            <SingleInput fieldName={'email'} fieldType={'email'} fieldLabel={'Email ID'} pholder={'@gmail.com'} require={true} inputChange={handleInputChange} inputValue={formInput.value} />
+                            <SingleInput fieldName={'email'} fieldType={'email'} fieldLabel={'Email ID'} pholder={'@gmail.com'} require={true} inputChange={handleInputChange} inputValue={formInput.email} />
                         </div>
                         <div>
-                            <SingleInput fieldName={'password'} fieldType={'password'} fieldLabel={'Password'} pholder={'*****'} require={true} inputChange={handleInputChange} inputValue={formInput.value} />
+                            <SingleInput fieldName={'password'} fieldType={'password'} fieldLabel={'Password'} pholder={'*****'} require={true} inputChange={handleInputChange} inputValue={formInput.password} />
                         </div>
                         <div className='my-6 flex justify-center'>
                             <button className='btn-primary' type="submit">SignUp</button>
