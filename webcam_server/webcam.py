95a19bd4c3782ecd9202cc6b573741e7d31f854a
Wasim Raja Middya <wasimmiddya21@gmail.com>
2023-10-29 13:05:54 +0530

    webcamp feature added

--- webcam_server/webcam.py
+++ webcam_server/webcam.py
@@ -1,27 +1,77 @@
-import cv2
-from flask import Flask, render_template, Response
+import cv2 as cv
+from flask import Flask, Response,jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='http://localhost:5173')
 
 # Access the webcam
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
+
+classifier = cv.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
 def generate_frames():
+    global cap 
+    
+    cap = cv.VideoCapture(0)
+
     while True:
         success, frame = cap.read()
+
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
 @app.route('/video_feed')
 def video_feed():
     print('Requested for video')
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+            
+
+@app.route('/check')
+def check():
+    global cap
+    cap = cv.VideoCapture(0)
+
+    count = 0
+    face = 0
+
+    print('Request for checking')
+    while True:
+        success, frame = cap.read()
+
+        if not success:
+            break
+        else:
+            blur = cv.GaussianBlur(frame, (3,3), cv.BORDER_DEFAULT)
+            detector = classifier.detectMultiScale(blur, scaleFactor=1.1, minNeighbors=10)
+
+            face = len(detector)
+
+
+            if face != 1:
+                count += 1
+            if face == 1:
+                count = 0
+            
+            if count >= 6:
+                    break
+    cap.release()
+    return jsonify({"msg":"VIOLENCE"})  
+          
+
+@app.route('/release')
+def rel(): 
+    global cap
+    cap.release()
+    return 'released'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
